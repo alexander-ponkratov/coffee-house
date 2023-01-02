d46813ab7a46b9dba70dcb887da921637ff4c234
alexander-ponkratov <alexander.ponkratov.n@gmail.com>
2023-01-02 16:52:02 +0700

    edit header

--- react.js
+++ react.js
@@ -6,13 +6,35 @@ function App() {
         display: ${props => props.disp};
         grid-template-columns: ${props => props.gridTemplateColumns};
     `
+    Menu = window.styled.div`
+
+    `
+    const Image = window.styled.img`
+
+    `
     return (
         <header>
-            <Container className='container' maxWidth="1200px">
-                <GridCol disp="grid" gridTemplateColumns="auto auto auto">123</GridCol>
+            <Container className='container' maxWidth="1764px">
+                <GridCol disp="grid" gridTemplateColumns="auto auto auto auto">
+                    <Image src='img/coffee-house-creative-logo-3B1F1F60F9-seeklogo 1.png'></Image>
+                    <Menu />
+                </GridCol>
             </Container>
         </header>
     );
+
+    function Menu(props) {
+        return (
+            <GridCol disp="grid" gridTemplateColumns='auto auto'>
+                <div>123</div>
+                <div>234</div>
+            </GridCol>
+        )
+    }
+
+    function Grid(props) {
+        
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
